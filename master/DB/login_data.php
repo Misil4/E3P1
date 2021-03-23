f293fa53c582fb8a3001afe99f4b3e3ea4e13c19
misil470gamer <carlossilvamarques.cs@gmail.com>
2021-03-23 12:13:50 +0100

    23-03-2021

---
+++ master/DB/login_data.php
@@ -0,0 +1,9 @@
+<?php
+
+//Datos de conexión con la base de datos
+$cfg['tipo_bd'] = 'mysql';
+$cfg['nombre_bd'] = 'publications';
+$cfg['servidor'] = 'localhost';
+$cfg['port'] = '80';
+$cfg['usuario'] = 'oscar1';
+$cfg['password'] = 'mysql123';
