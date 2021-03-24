ad1aa1a97eb93ba94d5d660f65ae96e9e675a8a6
misil470gamer <carlossilvamarques.cs@gmail.com>
2021-03-24 12:35:28 +0100

    24-03-2021

---
+++ master/controller/controller.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . "/../model/Classic.php";
+
+//Creamos nuestros modelos
+$classic = new Classic();
+$erabiltzaileak = new Erabiltzaileak();
+$produktuak = new Produktuak();
